6731e4174b4d2cc49abbc2d45a32647db54b4773
nao20010128nao <nao20010128@gmail.com>
2016-01-02 19:01:13 +0900

    何か消した

--- src/nao20010128nao/UHC.php
+++ src/nao20010128nao/UHC.php
@@ -171,13 +171,13 @@ class UHC extends PluginBase implements Listener
 			return false;
 		}
 	}
-	public function delTree($dir) { 
-		$files=array_diff(scandir($dir), array('.','..')); 
-		foreach ($files as $file) { 
-			(is_dir("$dir/$file") && !is_link($dir)) ?delTree("$dir/$file") :unlink("$dir/$file"); 
-		} 
-		return rmdir($dir); 
-	} 
+	public function delTree($dir) {
+		$files=array_diff(scandir($dir), array('.','..'));
+		foreach ($files as $file) {
+			(is_dir("$dir/$file") && !is_link($dir)) ?delTree("$dir/$file") :unlink("$dir/$file");
+		}
+		return rmdir($dir);
+	}
 	
 	  ////////////
 	 // EVENTS //
