7fcaf1b0529074e16f9ec330b8aa81e838a84649
nao20010128nao <nao20010128@gmail.com>
2016-01-03 01:00:20 +0900

    削除

--- src/nao20010128nao/block/Sapling.php
+++ src/nao20010128nao/block/Sapling.php
@@ -59,18 +59,6 @@ class Sapling extends Flowable{
 		return $names[$this->meta & 0x07];
 	}
 
-
-	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		$down = $this->getSide(0);
-		if($down->getId() === self::GRASS or $down->getId() === self::DIRT or $down->getId() === self::FARMLAND){
-			$this->getLevel()->setBlock($block, $this, true, true);
-
-			return true;
-		}
-
-		return false;
-	}
-
 	public function onActivate(Item $item, Player $player = null){
 		if($item->getId() === Item::DYE and $item->getDamage() === 0x0F){ //Bonemeal
 			//TODO: change log type
