64a087267a184707b57aef1e219079fad3eb308b
nao20010128nao <nao20010128@gmail.com>
2016-01-02 09:59:01 +0900

    ディレクトリ完全消去

--- src/nao20010128nao/UHC.php
+++ src/nao20010128nao/UHC.php
@@ -163,7 +163,13 @@ class UHC extends PluginBase implements Listener
 			return false;
 		}
 	}
-
+	public static function delTree($dir) { 
+		$files =array_diff(scandir($dir), array('.','..')); 
+		foreach ($files as $file) { 
+			(is_dir("$dir/$file") && !is_link($dir)) ?delTree("$dir/$file") :unlink("$dir/$file"); 
+		} 
+		return rmdir($dir); 
+	} 
 	
 	  ////////////
 	 // EVENTS //
